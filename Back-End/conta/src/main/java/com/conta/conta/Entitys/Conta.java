59365681533d0ec7ab2fdba956875e31f17c4806
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-08-01 20:03:18 -0300

    conta

--- Back-End/conta/src/main/java/com/conta/conta/Entitys/Conta.java
+++ Back-End/conta/src/main/java/com/conta/conta/Entitys/Conta.java
@@ -18,4 +18,117 @@ public class Conta {
     @Column(nullable = false)
     private LocalDate data;
 
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private float saldoTotal;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+    
+
+    public Conta(String proprietario, LocalDate data, String email, String telefone, String senha, float saldoTotal, String cpf, Boolean status) {
+        this.proprietario = proprietario;
+        this.data = data;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.saldoTotal = saldoTotal;
+        this.cpf = cpf;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public float getSaldoTotal() {
+        return saldoTotal;
+    }
+
+    public void setSaldoTotal(float saldoTotal) {
+        this.saldoTotal = saldoTotal;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "id=" + id +
+                ", proprietario='" + proprietario + '\'' +
+                ", data=" + data +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", senha='" + senha + '\'' +
+                ", saldoTotal=" + saldoTotal +
+                ", cpf='" + cpf + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
