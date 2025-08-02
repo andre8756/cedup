c9b5f78885d7b3b3026209970454a7fce23e45d3
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-08-01 22:08:18 -0300

    Controller

--- Back-End/conta/src/main/java/com/conta/conta/Controller/ContaController.java
+++ Back-End/conta/src/main/java/com/conta/conta/Controller/ContaController.java
@@ -2,8 +2,8 @@ package com.conta.conta.Controller;
 
 import com.conta.conta.Entity.Conta;
 import com.conta.conta.Service.ContaService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,6 +16,9 @@ public class ContaController {
 
     @Autowired
     private ContaService contaService;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -30,10 +33,23 @@ public class ContaController {
         return contaService.buscarPorId(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Conta não encontrada"));
     }
 
+    //Método Post para criação
     @PostMapping //Identifica que metodo a API deve executar ao fazer um POST
     @ResponseStatus(HttpStatus.CREATED) //Responde o resultado do post 201
     public Conta salvar(@RequestBody Conta conta){
         return contaService.salvar(conta);
+    }
+
+    // Método PUT para atualização
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarConta(@PathVariable("id") Long id, @RequestBody Conta conta){
+        contaService.buscarPorId(id)
+                .map(contaBase -> {
+                    modelMapper.map(conta, contaBase);
+                    contaService.salvar(contaBase);
+                    return Void.TYPE;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
     }
 
     @DeleteMapping("/{id}")
