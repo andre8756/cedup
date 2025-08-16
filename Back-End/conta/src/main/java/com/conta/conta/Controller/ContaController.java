bd990f40bdb89c0309820951f74ca9942563bd65
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-08-16 08:35:51 -0300

    implementando a entidade banco no controller

--- Back-End/conta/src/main/java/com/conta/conta/Controller/ContaController.java
+++ Back-End/conta/src/main/java/com/conta/conta/Controller/ContaController.java
@@ -1,6 +1,8 @@
 package com.conta.conta.Controller;
 
+import com.conta.conta.Entity.Banco;
 import com.conta.conta.Entity.Conta;
+import com.conta.conta.Service.BancoService;
 import com.conta.conta.Service.ContaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,12 @@ public class ContaController {
     private ContaService contaService;
 
     @Autowired
+    private BancoService bancoService;
+
+    @Autowired
     private ModelMapper modelMapper;
 
+    // Conta Principal -------------------------------------------------------------------------
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     public List<Conta> listaConta(){
@@ -61,6 +67,47 @@ public class ContaController {
         }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Conta não encontrada!"));
     }
 
+    // Outras contas bancárias -------------------------------------------------------------------------
 
+    @GetMapping("/banco")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Banco> listaBanco(){
+        return bancoService.listarBanco();
+    }
+
+    //Buscar banco específico
+    @GetMapping("/banco/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Banco buscarBanco(@PathVariable("id") Long id){
+        return bancoService.buscarPorId(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Banco não encontrada"));
+    }
+
+    //Método Post para criação
+    @PostMapping("/banco") //Identifica que metodo a API deve executar ao fazer um POST
+    @ResponseStatus(HttpStatus.CREATED) //Responde o resultado do post 201
+    public Banco salvar(@RequestBody Banco banco){
+        return bancoService.salvar(banco);
+    }
+
+    // Método PUT para atualização
+    @PutMapping("/banco/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void atualizarBanco(@PathVariable("id") Long id, @RequestBody Banco banco){
+        bancoService.buscarPorId(id)
+                .map(bancoBase -> {
+                    modelMapper.map(banco, bancoBase);
+                    bancoService.salvar(bancoBase);
+                    return Void.TYPE;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Banco nao encontrado"));
+    }
+
+    @DeleteMapping("/banco/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerBanco(@PathVariable("id") Long id){
+        bancoService.buscarPorId(id).map(banco -> {
+            bancoService.removerPorId(banco.getId());
+            return Void.TYPE;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Banco não encontrada!"));
+    }
 
 }
