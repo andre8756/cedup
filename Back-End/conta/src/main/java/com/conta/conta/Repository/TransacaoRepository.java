245e6792bd1b4583c5e5aa673eaa615623308515
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-09-02 10:17:27 -0300

    transacaoRepository

--- Back-End/conta/src/main/java/com/conta/conta/Repository/TransacaoRepository.java
+++ Back-End/conta/src/main/java/com/conta/conta/Repository/TransacaoRepository.java
@@ -1,4 +1,20 @@
 package com.conta.conta.Repository;
 
-public interface TransacaoRepository {
+import com.conta.conta.Entity.Transacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+    List<Transacao> findByContaId(Long contaId);
+
+    @Query("SELECT t FROM Transacao t WHERE t.bancoOrigem.id = :bancoId OR t.bancoDestino.id = :bancoId")
+    List<Transacao> findByBancoOrigemIdOrBancoDestinoId(@Param("bancoId") Long bancoId,
+                                                        @Param("bancoId") Long bancoId2);
+
+    List<Transacao> findByDataTransacaoBetween(LocalDateTime inicio, LocalDateTime fim);
 }
