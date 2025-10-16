21d7f63cf47b8d87b7b0ee5faa37e841aa9c132e
Jose Lourenço Neto <141178650+Piclisambulante@users.noreply.github.com>
2025-10-15 22:01:48 -0300

    Update PopupAdd.tsx

--- Front-End/src/components/PopupAdd/PopupAdd.tsx
+++ Front-End/src/components/PopupAdd/PopupAdd.tsx
@@ -1,1 +1,80 @@
+import React, { useState } from "react";
 
+interface PopupFormProps {
+  onClose: () => void;
+  onSubmit: (novaConta: { titular: string; instituicao: string; saldo: number }) => void;   
+}
+
+const PopupForm: React.FC<PopupFormProps> = ({ onClose }) => {
+  const [formData, setFormData] = useState({
+    titular: "",
+    instituicao: "",
+    saldo: "",
+    numero:""
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/add", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert("Conta adicionada com sucesso!");
+        onClose();
+      } else {
+        alert("Erro ao adicionar conta.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Erro de conexão com o servidor.");
+    }
+  };
+
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <h1>Adicionar Conta</h1>
+        
+        <label htmlFor="titular">Titular da Conta:</label>
+        <input
+          id="titular"
+          value={formData.titular}
+          onChange={handleChange}
+          placeholder="Ex: Lucas Andrade"
+        />
+
+        <label htmlFor="instituicao">Nome da Instituição Bancária:</label>
+        <input
+          id="instituicao"
+          value={formData.instituicao}
+          onChange={handleChange}
+          placeholder="Ex: Inter"
+        />
+
+        <label htmlFor="saldo">Informe o Saldo Bancário:</label>
+        <input
+          id="saldo"
+          type="number"
+          step="0.01"
+          value={formData.saldo}
+          onChange={handleChange}
+          placeholder="Ex: R$6312,48"
+        />
+
+        <div className="popup-buttons">
+          <button onClick={handleSubmit}>Adicionar Conta</button>
+          <button onClick={onClose}>Cancelar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopupForm;
