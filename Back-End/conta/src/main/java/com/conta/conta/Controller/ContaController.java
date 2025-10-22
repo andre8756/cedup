4b7a7be1bc2df0e205c575f153b1cb998f992ef6
Jose Lourenço Neto <141178650+Piclisambulante@users.noreply.github.com>
2025-10-21 23:03:44 -0300

    Update ContaController.java

--- Back-End/conta/src/main/java/com/conta/conta/Controller/ContaController.java
+++ Back-End/conta/src/main/java/com/conta/conta/Controller/ContaController.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5137")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/conta")
 public class ContaController {
