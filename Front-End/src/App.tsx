80be5511cb7c606282cf4db5e0abd72521581f3e
nicolasRotta1 <142179878+nicolasRotta1@users.noreply.github.com>
2025-11-05 14:20:41 -0300

    Corrigindo Css

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -5,12 +5,10 @@ import AppRoutes from './routes/AppRoutes'
 
 function App() {
   useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('App mounted');
   }, [])
 
   return (
-    <div className="app">
+    <div id="app">
       <AppRoutes />
     </div>
   )
--- Front-End/tailwind.config.cjs
+++
@@ -1,5 +0,0 @@
-
-// tailwind.config.cjs - removed
-// Tailwind has been intentionally removed from this project.
-// If you want to re-enable Tailwind, restore a proper configuration and
-// install the `tailwindcss` package.
