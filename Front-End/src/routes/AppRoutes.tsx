541c1ce229c1d10c242d7455b350d63e990dc5ca
nicolasRotta1 <142179878+nicolasRotta1@users.noreply.github.com>
2025-11-10 16:11:20 -0300

    feat - dashboard

--- Front-End/src/routes/AppRoutes.tsx
+++ Front-End/src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import Cadastro from '../pages/Cadastro/Cadastro';
 import Conta from '../pages/Conta/Conta';
+import Dashboard from '../pages/Dashboard/Dashboard';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path='/dashboard' element={<Dashboard />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/conta" element={<Conta />} />
